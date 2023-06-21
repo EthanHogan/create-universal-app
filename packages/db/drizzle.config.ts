1fd6ff82a2b54d405383c589182a960a466a12b4
Ethan <ethan@bombshelltechnologies.com>
2023-06-20 22:03:34 -0500

    changes drizzle config to just use connectionString

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,13 +1,9 @@
 import type { Config } from "drizzle-kit";
-import * as dotenv from "dotenv";
-dotenv.config();
+// import * as dotenv from "dotenv";
+// dotenv.config();
 
 export default {
   schema: "./drizzle/schema.ts",
   out: "./drizzle/generated",
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_SCHEMA,
-} as Config;
+  connectionString: process.env.DATABASE_URL || "",
+} satisfies Config;
